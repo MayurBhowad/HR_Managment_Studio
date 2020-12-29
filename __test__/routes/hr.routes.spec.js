44199ba8f208215dce107ba0b2b64e5df7649c20
MayurBhowad <mayurbhowad1995@gmail.com>
2020-12-29 15:17:13 +0530

    testing hr route

--- __test__/routes/hr.routes.spec.js
+++ __test__/routes/hr.routes.spec.js
@@ -3,14 +3,31 @@ const request = require('supertest');
 const app = require('../../app.main');
 const conn = require('../../db/db.main');
 
+const HR = require('../../db/models/hr.model');
+
 describe('hr route', () => {
     beforeAll((done) => {
         conn.connect()
-            .then(() => done())
+            .then(() => {
+                const newTestHr = new HR({
+                    added_by: '5fe9dc614c7b133684960471',
+                    first_name: 'tester',
+                    last_name: 'testuser',
+                    email: 'testuser@gmail.com',
+                    password: '123456',
+                    cpassword: '123456',
+                    dob: '1995-12-12',
+                    hr_no: '100',
+                    permission: 'admin'
+                })
+                newTestHr.save().then(() => done());
+
+            })
             .catch(err => done(err));
     })
-    afterAll((done) => {
-        conn.close()
+    afterAll(async (done) => {
+        // conn.close()
+        await HR.dropCollection()
             .then(() => done())
             .catch(err => done(err));
     });
@@ -35,7 +52,7 @@ describe('hr route', () => {
     it('expected success!', (done) => {
         request(app).post('/hr/add')
             .send({
-                added_by: '',
+                added_by: '5feaf73a33631231d89e76dc',
                 first_name: 'testuser',
                 last_name: 'testuser',
                 email: 'testuser@gmail.com',
