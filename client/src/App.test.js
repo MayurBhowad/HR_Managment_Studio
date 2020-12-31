395100857e4ec68bdee6695da405b5f7f2ce541c
MayurBhowad <mayurbhowad1995@gmail.com>
2020-12-31 23:43:02 +0530

    github workflow

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,5 +1,7 @@
-import { render, screen } from '@testing-library/react';
+import { cleanup, render, screen } from '@testing-library/react';
 import App from './App';
+
+afterEach(cleanup)
 
 test('renders learn react link', () => {
   render(<App />);
